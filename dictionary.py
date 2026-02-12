0d52c51f65a73e7dc56c3de3adeeedab82336ad8
mt-blip34 <masnoontahmid@iut-dhaka.edu>
2026-02-12 23:47:39 +0600

    Remove unwanted files from repository

--- dictionary.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-# Define your metrics from training
-classes = ['bottle', 'polythene', 'styrofoam']
-mAP50 = [0.578, 0.745, 0.794]
-mAP50_95 = [0.512, 0.612, 0.365]
-
-# You can also add overall metrics if you like
-overall = {'mAP50': 0.706, 'mAP50_95': 0.496}
